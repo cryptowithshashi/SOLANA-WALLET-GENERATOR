bd6dd8aa152a6af6a44bd27d8049206354252351
CRYPTO WITH SHASHI <shashis697723@gmail.com>
2025-04-02 09:23:39 +0530

    Create banner.js

---
+++ banner.js
@@ -0,0 +1,19 @@
+import chalk from 'chalk';
+
+// ASCII Art Banner for the EVM Wallet Generator
+export const banner = `
+                         
+
+ ██████╗            ██╗    ██╗        ███████╗              
+██╔════╝            ██║    ██║        ██╔════╝
+██║                 ██║ █╗ ██║        ███████╗
+██║                 ██║███╗██║        ╚════██║
+╚██████╗            ╚███╔███╔╝        ███████║
+ ╚═════╝             ╚══╝╚══╝         ╚══════╝
+
+ -___________________________________________________________________________-                                                           
+
+ 📢 ${chalk.cyan('Telegram Channel:')} ${chalk.underline.blue('https://t.me/crypto_with_shashi')} 
+ 📢 ${chalk.cyan('Twitter Handle:')} ${chalk.underline.blue('https://x.com/SHASHI522004')}
+ 📢 ${chalk.cyan('Github:')} ${chalk.underline.blue('https://github.com/cryptowithshashi')}
+`;
